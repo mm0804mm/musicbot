7982914c2be4f651887bd06ba40966c200b18d47
mm0804mm <mm0804mm@naver.com>
2021-04-02 10:54:02 +0900

    v.1
    .

---
+++ bot.py
@@ -0,0 +1,40 @@
+import youtube_dl
+import discord
+
+intents = discord.Intents.default()
+client = discrod.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('login')
+    print(client.user.id)
+    print('------------------------')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("/들어와 현성"):
+        await message.author.voice.channel.connect()
+        await message.channel.send("두둥등장")
+
+    if message.content.startswith("/돌아가 현성"):
+        for vc in client.voice_clients:
+            if vc.guild == message.guild:
+                voice = vc
+        await voice.disconnect()
+        await message.channel.sned("도주~")
+
+        url = message.content.split(" ")[1]
+        option = {
+            'format': 'bestaudio/best','postprocessors':[{'preferredcodec':'mp3','key':'FFmpegExtraAudio','preferredquality':'320',},'outtmpl':"file/"+url.split('=')[1]+'.mp3'}
+            
+            
+        }
+
+        with youtube_dl.YoutubeDL(option) as ydl:
+            ydl.download(url)
+            info = ydl.extract_info(url, download=False)
+            title = info["title"]
+
+        voice.play(discord.FFmpegPCMAudio("file/" + url.split('=')[1] + ".mp3"))
+        await message.channel.send(title + "을 듣는다고? 뭔 또 버러지같은 노래야?")
