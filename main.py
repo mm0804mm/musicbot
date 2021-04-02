57384bd647e75e48d9046b0b91c2bb55656a37bc
mm0804mm <mm0804mm@naver.com>
2021-04-02 11:29:09 +0900

    v.3
    .

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import youtube_dl
 import discord
-
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
@@ -8,32 +7,33 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print('login')
     print(client.user.id)
-    print('------------------------')
-
+    print('---------------------')
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("/들어와 현성"):
+    if message.content.startswith("/입장"):
         await message.author.voice.channel.connect()
         await message.channel.send("두둥등장")
-
-    if message.content.startswith("/돌아가 현성"):
+    if message.content.startswitch("/퇴장"):
         for vc in client.voice_clients:
             if vc.guild == message.guild:
                 voice = vc
         await voice.disconnect()
-        await message.channel.sned("도주~")
+        await message.channel.send("도주")
 
+    if message.content.startswitch("/재생"):
+        for vc in client.voice_clients:
+            if vc.guild == message.guild:
+                voice = vc
         url = message.content.split(" ")[1]
         option = {
-            'outtmpl':"file/"+url.split('=')[1]+".mp3"
-            
+            'outtmpl' : "file/" + url.split('=')[1] + ".mp3"
         }
 
         with youtube_dl.YoutubeDL(option) as ydl:
             ydl.download(url)
             info = ydl.extract_info(url, download=False)
             title = info["title"]
-
+        
         voice.play(discord.FFmpegPCMAudio("file/" + url.split('=')[1] + ".mp3"))
-        await message.channel.send(title + "을 듣는다고? 뭔 또 버러지같은 노래야?")
+        await message.channel.send(title + "을 듣는다고? 개버러지네.")
